1a7592670807920d2095b4d6b09d18b800c40a4f
FVS-MBA <feliven@gmail.com>
2026-04-04 18:03:05 -0300

    implementa putLivro e deleteLivro

--- api/src/controllers/LivroController.ts
+++ api/src/controllers/LivroController.ts
@@ -36,3 +36,51 @@ export const postLivro = (req: Request, res: Response) => {
     }
   }
 };
+
+export const putLivro = (req: Request<Livro>, res: Response) => {
+  try {
+    const dbLivros: Livro[] = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+
+    const livroAtualizado: Livro = req.body;
+    const indexLivroParaAtualizar = dbLivros.findIndex((livro) => livro.id === livroAtualizado.id);
+
+    dbLivros[indexLivroParaAtualizar] = { ...livroAtualizado };
+    fs.writeFileSync(dbPath, JSON.stringify(dbLivros));
+
+    res.status(201).json(dbLivros);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send(`An unexpected error occurred: ${error as string}`);
+    }
+  }
+};
+export const deleteLivro = (req: Request<{ id: string }>, res: Response) => {
+  try {
+    const dbLivros: Livro[] = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+
+    const { id } = req.body;
+    if (!id) {
+      res.status(400).json({ message: "ID é obrigatório" });
+      return;
+    }
+
+    const indexLivroParaRemover = dbLivros.findIndex((livro) => livro.id === Number(id));
+    if (indexLivroParaRemover === -1) {
+      res.status(404).json({ message: "Livro não encontrado" });
+      return;
+    }
+
+    dbLivros.splice(indexLivroParaRemover, 1);
+    // fs.writeFileSync(dbPath, JSON.stringify(dbLivros));
+
+    res.status(200).json(dbLivros);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send(`An unexpected error occurred: ${error as string}`);
+    }
+  }
+};
--- api/src/routes/Livros.routes.ts
+++ api/src/routes/Livros.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { getLivros, postLivro } from "../controllers/LivroController";
+import { getLivros, postLivro, putLivro, deleteLivro } from "../controllers/LivroController";
 
 const router = Router();
 
 router.get("/", getLivros);
 
 router.post("/", postLivro);
+
+router.put("/", putLivro);
+
+router.delete("/", deleteLivro);
 
 export default router;
